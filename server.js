8210ee86a94a5d91f45a4a43a059518a730b4acc
Shrike6 <cody.stubbs@gmail.com>
2022-08-17 09:05:10 -0500

    added the rest of the GET ROUTES

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
+// Dependencies
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const fs = require('fs');
+
 
 const PORT = 3001;
 
 const app = express();
 
-// MIddleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-// GET route for html
+// GET route for html main page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+// GET route for notes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let notelength = (noteList.length).toString();
+
+  newNote.id = notelength;
+  noteList.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+  res.json(noteList);
+});
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
